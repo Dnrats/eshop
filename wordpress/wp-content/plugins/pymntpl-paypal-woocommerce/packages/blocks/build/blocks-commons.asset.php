814a2595b88c5adc6bbe47d3ab5100a5307b47d7
artem <artemfr@hotmail.com>
2024-02-15 04:37:22 +0100

    Adding products to the shop

---
+++ wordpress/wp-content/plugins/pymntpl-paypal-woocommerce/packages/blocks/build/blocks-commons.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '1e240553b3707b6d3bce');
---
+++ wordpress/wp-content/plugins/pymntpl-paypal-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4ff7500b56acb9036076afd03ba0733cede486d5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4ff7500b56acb9036076afd03ba0733cede486d5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'paymentplugins/paypal-php-sdk' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8f3098be2f0d965237974f0a4626a0c1d1539aa0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../paymentplugins/paypal-php-sdk',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
